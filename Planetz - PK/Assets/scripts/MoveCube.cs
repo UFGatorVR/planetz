0b8acf4020c818b167c85f54f160ab32adb5a7f4
Parker Knight <parkerknight@Parkers-MacBook-Air.local>
2017-10-14 12:53:33 -0400

    Add textures and assets

---
+++ Planetz - PK/Assets/scripts/MoveCube.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//FINISH
+public class MoveCube : MonoBehaviour {
+	public GravityBody player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag ("Player").GetComponent<GravityBody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Planetz - PK/Assets/scripts/PlanetSize.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (GravityAttractor))]
+public class PlanetSize : MonoBehaviour {
+	public float radius = 50;
+	// Use this for initialization
+
+	void Start () {
+		transform.localScale = new Vector3 (radius * 2, radius * 2, radius * 2);
+	}
+
+}
