87c318d3f41925536b9487d6b451415195e73835
Parker Knight <parkerknight@Parkers-MacBook-Air.local>
2017-10-14 17:21:53 -0400

    Forward jumping

--- Planetz - PK/Assets/scripts/FirstPerson.cs
+++ Planetz - PK/Assets/scripts/FirstPerson.cs
@@ -8,16 +8,21 @@ public class FirstPerson : MonoBehaviour {
 	public float mouseSensitivityX = 1;
 	public float mouseSensitivityY = 1;
 	public float walkSpeed = 6;
-	public float jumpForce = 220;
+	public float jumpForce = 200;
 	public LayerMask groundedMask;
 
-
+	GravityAttractor planet;
 	bool grounded;
 	Vector3 moveAmount;
 	Vector3 smoothMoveVelocity;
 	float verticalLookRotation;
 	Rigidbody rb;
 	Transform cameraTransform;
+	private int jumpCount;
+	Vector3 playerPos;
+	Vector3 planetPos;
+	Vector3 planetSize;
+
 	// Use this for initialization
 
 	void Start () {
@@ -25,6 +30,15 @@ public class FirstPerson : MonoBehaviour {
 		Cursor.visible = false;
 		cameraTransform = Camera.main.transform;
 		rb = GetComponent<Rigidbody> ();
+		jumpCount = 0;
+		playerPos = rb.transform.position;
+		grounded = true;
+		planet = GameObject.FindWithTag ("Planet").GetComponent<GravityAttractor> ();
+
+		planetPos = planet.transform.position;
+		planetSize = planet.transform.localScale;
+
+
 		
 	}
 	
@@ -42,27 +56,36 @@ public class FirstPerson : MonoBehaviour {
 		Vector3 targetMoveAmount = moveDir * walkSpeed;
 		moveAmount = Vector3.SmoothDamp(moveAmount,targetMoveAmount,ref smoothMoveVelocity,.15f);
 
-		if (Input.GetButtonDown("Jump")) {
-			if (grounded) {
-				rb.AddForce(transform.up * jumpForce);
+		if ((playerPos.x <= (planetPos.x + planetSize.x/2 + 10)) && 
+			(playerPos.y <= (planetPos.y + planetSize.y/2 + 10)) &&
+			(playerPos.z <= (planetPos.z + planetSize.z/2 + 10))){
+				grounded = true;
 			}
+
+		if (grounded) {
+			jumpCount = 0;
 		}
 
-		// Grounded check
-		Ray ray = new Ray(transform.position, -transform.up);
-		RaycastHit hit;
 
-		if (Physics.Raycast(ray, out hit, 1 + .1f, groundedMask)) {
-			grounded = true;
-		}
-		else {
-			grounded = false;
-		}
+			
 	}
 
 	void FixedUpdate() {
 		// Apply movement to rigidbody
 		Vector3 localMove = transform.TransformDirection(moveAmount) * Time.fixedDeltaTime;
 		rb.MovePosition(rb.position + localMove);
+		if (Input.GetKeyDown("space") && jumpCount < 3) {
+			rb.AddForce (transform.up * jumpForce);
+			jumpCount++;
+			grounded = false;
+
+		}
+
+		if (Input.GetKeyDown ("c") && jumpCount < 3) {
+			rb.AddForce (transform.forward * jumpForce);
+			grounded = false;
+			jumpCount++;
+
+		}
 	}
 }
